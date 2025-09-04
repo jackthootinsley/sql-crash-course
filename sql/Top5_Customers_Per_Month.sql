30b5fa3349d48d4ca14c85b04d5f02ca8f67c0db
jacktins98 <jacktinsley98@gmail.com>
2025-09-04 09:47:32 +0100

    Top 5 Customers Per Month
    Demonstrates use of aggregation, window function, and date truncation

---
+++ sql/Top5_Customers_Per_Month.sql
@@ -0,0 +1,34 @@
+/*
+Calculates the top 5 customers by total spend for each month.
+Demonstrates use of aggregation, window functions, and date truncation.
+*/
+
+WITH customer_monthly_spend_cte AS (
+	SELECT
+		"CustomerID",
+		DATE_TRUNC('month', "InvoiceDate"::timestamp) AS month,
+		SUM("Quantity" * "UnitPrice"::numeric) AS total_spend
+	FROM kaggle_ecommerce_dataset AS kaggle
+	WHERE "CustomerID" <> ''
+	GROUP BY "CustomerID", month
+)
+SELECT
+	"CustomerID",
+	month,
+	ROUND(total_spend, 2) AS total_spend
+FROM (
+	SELECT 
+		"CustomerID",
+		month,
+		total_spend,
+		ROW_NUMBER() OVER (PARTITION BY month ORDER BY total_spend DESC) as ranking
+	FROM customer_monthly_spend_cte
+) AS ranked
+WHERE ranking <= 5
+ORDER BY month, total_spend DESC;
+
+
+
+
+
+	
